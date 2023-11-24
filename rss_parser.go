86273e97c41a691e7a9abd387ec5be6678dfe19c
dlworhd <dlwodud821@gmail.com>
2023-11-24 14:43:31 +0900

    fix: Remove Main Function

--- rss_parser.go
+++ rss_parser.go
@@ -20,42 +20,6 @@ type Feed struct {
 	Published   string
 }
 
-// func main() {
-// 	wg := new(sync.WaitGroup)
-// 	fp := gofeed.NewParser()
-
-// 	rssUrls := []string{
-// 		"https://toss.tech/rss.xml",
-// 	}
-
-// 	wg.Add(len(rssUrls))
-
-// 	var outerFeedArr [][]Feed
-
-// 	for range rssUrls {
-// 		outerFeedArr = append(outerFeedArr, nil)
-// 	}
-
-// 	for i, rss := range rssUrls {
-// 		ProceedFeed(fp, rss, wg, outerFeedArr, i)
-// 	}
-
-// 	for _, innerFeedArr := range outerFeedArr {
-// 		for _, feed := range innerFeedArr {
-// 			fmt.Println("GUID: ", feed.Guid)
-// 			fmt.Println("Title: ", feed.Title)
-// 			fmt.Println("Thumbnail: ", feed.Thumbnail)
-// 			fmt.Println("Description : ", feed.Description)
-// 			fmt.Println("Link: ", feed.Link)
-// 			fmt.Println("Published: ", feed.Published)
-// 			fmt.Printf("\n")
-// 		}
-// 	}
-
-// 	wg.Wait()
-
-// }
-
 func ProceedFeed(fp *gofeed.Parser, rss string, wg *sync.WaitGroup, outerFeedArr [][]Feed, outIndex int) {
 	innerWg := new(sync.WaitGroup)
 
