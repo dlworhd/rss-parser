d598cc5919dca3f51cd0099bc628b442681ad9c0
dlworhd <dlwodud821@gmail.com>
2023-11-24 13:57:27 +0900

    fix: Modify Private Variables To Public

--- rss_parser.go
+++ rss_parser.go
@@ -1,6 +1,7 @@
 package rss_parser
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -12,49 +13,49 @@ import (
 )
 
 type Feed struct {
-	guid        string
-	title       string
-	thumbnail   string
-	description string
-	link        string
-	published   string
+	Guid        string
+	Title       string
+	Thumbnail   string
+	Description string
+	Link        string
+	Published   string
 }
 
-// func main() {
-// 	wg := new(sync.WaitGroup)
-// 	fp := gofeed.NewParser()
+func main() {
+	wg := new(sync.WaitGroup)
+	fp := gofeed.NewParser()
 
-// 	rssUrls := []string{
-// 		"https://toss.tech/rss.xml",
-// 	}
+	rssUrls := []string{
+		"https://toss.tech/rss.xml",
+	}
 
-// 	wg.Add(len(rssUrls))
+	wg.Add(len(rssUrls))
 
-// 	var outerFeedArr [][]Feed
+	var outerFeedArr [][]Feed
 
-// 	for range rssUrls {
-// 		outerFeedArr = append(outerFeedArr, nil)
-// 	}
+	for range rssUrls {
+		outerFeedArr = append(outerFeedArr, nil)
+	}
 
-// 	for i, rss := range rssUrls {
-// 		ProceedFeed(fp, rss, wg, outerFeedArr, i)
-// 	}
+	for i, rss := range rssUrls {
+		ProceedFeed(fp, rss, wg, outerFeedArr, i)
+	}
 
-// 	for _, innerFeedArr := range outerFeedArr {
-// 		for _, feed := range innerFeedArr {
-// 			fmt.Println("GUID: ", feed.guid)
-// 			fmt.Println("Title: ", feed.title)
-// 			fmt.Println("Thumbnail: ", feed.thumbnail)
-// 			fmt.Println("Description : ", feed.description)
-// 			fmt.Println("Link: ", feed.link)
-// 			fmt.Println("Published: ", feed.published)
-// 			fmt.Printf("\n")
-// 		}
-// 	}
+	for _, innerFeedArr := range outerFeedArr {
+		for _, feed := range innerFeedArr {
+			fmt.Println("GUID: ", feed.Guid)
+			fmt.Println("Title: ", feed.Title)
+			fmt.Println("Thumbnail: ", feed.Thumbnail)
+			fmt.Println("Description : ", feed.Description)
+			fmt.Println("Link: ", feed.Link)
+			fmt.Println("Published: ", feed.Published)
+			fmt.Printf("\n")
+		}
+	}
 
-// 	wg.Wait()
+	wg.Wait()
 
-// }
+}
 
 func ProceedFeed(fp *gofeed.Parser, rss string, wg *sync.WaitGroup, outerFeedArr [][]Feed, outIndex int) {
 	innerWg := new(sync.WaitGroup)
@@ -104,7 +105,7 @@ func ExtractThumbnail(htmlContent string) (string, error) {
 		return "", err
 	}
 
-	var thumbnailURL string
+	var ThumbnailURL string
 	var traverseFunc func(*html.Node)
 	traverseFunc = func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == "meta" {
@@ -112,7 +113,7 @@ func ExtractThumbnail(htmlContent string) (string, error) {
 				if attr.Key == "property" && attr.Val == "og:image" {
 					for _, subAttr := range n.Attr {
 						if subAttr.Key == "content" {
-							thumbnailURL = subAttr.Val
+							ThumbnailURL = subAttr.Val
 							return
 						}
 					}
@@ -126,7 +127,7 @@ func ExtractThumbnail(htmlContent string) (string, error) {
 	}
 	traverseFunc(doc)
 
-	return thumbnailURL, nil
+	return ThumbnailURL, nil
 }
 
 func AddFeedItem(feed *gofeed.Item, innerWg *sync.WaitGroup, feedArr *[]Feed) {
@@ -137,18 +138,18 @@ func AddFeedItem(feed *gofeed.Item, innerWg *sync.WaitGroup, feedArr *[]Feed) {
 		panic(err)
 	}
 	if err != nil {
-		log.Printf("Error extracting thumbnail for %s: %v", feed.Link, err)
+		log.Printf("Error extracting Thumbnail for %s: %v", feed.Link, err)
 		panic(err)
 	}
-	thumbnail, _ := ExtractThumbnail(html)
+	Thumbnail, _ := ExtractThumbnail(html)
 
 	f := Feed{
-		guid:        feed.GUID,
-		title:       feed.Title,
-		thumbnail:   thumbnail,
-		description: feed.Description,
-		link:        feed.Link,
-		published:   feed.Published,
+		Guid:        feed.GUID,
+		Title:       feed.Title,
+		Thumbnail:   Thumbnail,
+		Description: feed.Description,
+		Link:        feed.Link,
+		Published:   feed.Published,
 	}
 
 	*feedArr = append(*feedArr, f)
